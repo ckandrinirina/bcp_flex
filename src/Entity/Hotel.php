fc65b59da9fb7c4656f840a7ccc5e8c4550a8cd3
ckandrinirina <ckandrinirina@gmail.com>
2020-04-06 11:38:03 +0300

    push

--- src/Entity/Hotel.php
+++ src/Entity/Hotel.php
@@ -6,7 +6,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\HotelRepository")
@@ -23,7 +22,6 @@ class Hotel
 
     /**
      * @ORM\Column(type="string", length=255, unique=true)
-     * @Assert\Unique(message="Unique")
      */
     private $nom;
 
